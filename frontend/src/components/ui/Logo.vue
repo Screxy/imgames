23803c41821c2a526e8c8c21a03b686e4038fd0e
Nikita Glazkov <zitrnik@gmail.com>
2022-06-19 00:48:46 +0300

    DEV: add logo short version

--- frontend/src/components/ui/Logo.vue
+++ frontend/src/components/ui/Logo.vue
@@ -1,10 +1,31 @@
 <template>
-  <img src="@/assets/logo.png" alt="Лого" />
+  <div>
+    <img v-if="windowWidth > 610" src="@/assets/logo.png" alt="Лого" />
+    <img v-else src="@/assets/logo_short.png" alt="Лого" />
+  </div>
 </template>
 
 <script>
 export default {
   name: 'Logo',
+  data() {
+    return {
+      windowWidth: window.innerWidth,
+    };
+  },
+  methods: {
+    onResize() {
+      this.windowWidth = window.innerWidth;
+    },
+  },
+  mounted() {
+    this.$nextTick(() => {
+      window.addEventListener('resize', this.onResize);
+    });
+  },
+  beforeDestroy() {
+    window.removeEventListener('resize', this.onResize);
+  },
 };
 </script>
 
@@ -12,5 +33,11 @@ export default {
 img {
   height: 48px;
   display: block;
+}
+
+@media screen and (max-width: 610px) {
+  img {
+    margin-left: 0.5rem;
+  }
 }
 </style>
